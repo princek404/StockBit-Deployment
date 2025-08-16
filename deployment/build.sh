c41536426964460451032f276fe7f46f287c3e94
princek404 <princedagogoekine@gmail.com>
2025-08-16 23:35:01 +0100

    Update build.sh

--- deployment/build.sh
+++ deployment/build.sh
@@ -10,14 +10,3 @@ pip install -r requirements.txt
 
 # Run database migrations
 flask db upgrade
-
-# Create tables if migrations fail
-python -c "
-from app import app, db
-with app.app_context():
-    try:
-        db.create_all()
-        print('Database tables created successfully')
-    except Exception as e:
-        print(f'Error creating tables: {str(e)}')
-"
