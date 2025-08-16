c0bb35fb10eac83c8131754645b71691b271dac6
princek404 <princedagogoekine@gmail.com>
2025-08-16 21:59:26 +0100

    Update build.sh

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-set -e  # Exit immediately on error
+set -e
+
+cd deployment
+
+# Create required directories
+mkdir -p instance
+mkdir -p uploads
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Run migrations
 flask db upgrade
-
-# Create necessary directories
-mkdir -p instance
-mkdir -p uploads
